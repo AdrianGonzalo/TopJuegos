a02f5cf3038785ee2fe91ee75c0aac2db5a00ebc
Adrian <adriangonzaloma@gmail.com>
2024-06-11 10:08:36 +0200

    complete the funcionality, it remains to be filled with games #1

---
+++ TOPJUEGOS/src/AllGames.jsx
@@ -0,0 +1,67 @@
+import './App.css'
+
+function AllGames(){
+    return <div>
+    <h1 className='Title'>TOP JUEGOS</h1>
+    <div>
+        <div className='AllGames'>
+            <section className='Game'>
+                <img src='https://image.api.playstation.com/vulcan/ap/rnd/202302/2321/3098481c9164bb5f33069b37e49fba1a572ea3b89971ee7b.jpg' className='Image' />
+
+                <div className='AllDescription'>
+                    <p>Baldur's Gate 3</p>
+
+                    <p className='Grade100-90'>10</p>
+                </div>
+            </section>
+            <section className='Game'>
+                <img src='https://media.contentapi.ea.com/content/dam/masseffect/images/2020/10/me-featured-image-mele-keyart-logo.jpg.adapt.crop16x9.1023w.jpg' className='Image' />
+
+                <div className='AllDescription'>
+                    <p>Mass Effect</p>
+
+                    <p className='Grade100-90'>10</p>
+                </div>
+            </section>
+            <section className='Game'>
+                <img src='https://image.api.playstation.com/vulcan/ap/rnd/202107/1612/Y5RHNmzAtc6sRYwZlYiKHAxN.png' className='Image' />
+
+                <div className='AllDescription'>
+                    <p>Elden Ring</p>
+
+                    <p className='Grade100-90'>9.5</p>
+                </div>
+            </section>
+            <section className='Game'>
+                <img src='https://image.api.playstation.com/vulcan/ap/rnd/202302/2321/3098481c9164bb5f33069b37e49fba1a572ea3b89971ee7b.jpg' className='Image' />
+
+                <div className='AllDescription'>
+                    <p>Baldur's Gate 3</p>
+
+                    <p className='Grade40-30'>10</p>
+                </div>
+            </section>
+            <section className='Game'>
+                <img src='https://image.api.playstation.com/vulcan/ap/rnd/202302/2321/3098481c9164bb5f33069b37e49fba1a572ea3b89971ee7b.jpg' className='Image' />
+
+                <div className='AllDescription'>
+                    <p>Baldur's Gate 3</p>
+
+                    <p className='Grade40-30'>10</p>
+                </div>
+            </section>
+            <section className='Game'>
+                <img src='https://image.api.playstation.com/vulcan/ap/rnd/202302/2321/3098481c9164bb5f33069b37e49fba1a572ea3b89971ee7b.jpg' className='Image' />
+
+                <div className='AllDescription'>
+                    <p>Baldur's Gate 3</p>
+
+                    <p className='Grade40-30'>10</p>
+                </div>
+            </section>
+        </div>
+    </div>
+</div>
+}
+
+export default AllGames
---
+++ TOPJUEGOS/src/App.jsx
@@ -0,0 +1,8 @@
+import './App.css'
+import AllGames from './AllGames'
+
+function App() {
+    return <AllGames />
+}
+
+export default App
